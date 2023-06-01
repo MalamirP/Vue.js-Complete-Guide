fed46d0214e38dfc5f1a36446ca5c6aac64240f1
Malamir P <mimoe@abv.bg>
2023-06-01 12:56:27 +0300

    surrender button funcionality and show different section depend the game is finished or not

--- prj-monster-01-starting-setup/prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/prj-monster-01-starting-setup/app.js
@@ -15,9 +15,15 @@ const app = Vue.createApp({
     computed:
     {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return { width: '0%' }
+            }
             return { width: this.monsterHealth + '%' }
         },
         playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return { width: '0%' }
+            }
             return { width: this.playerHealth + '%' }
         },
         mayUseSpecialAttack() {
@@ -26,6 +32,12 @@ const app = Vue.createApp({
     },
 
     methods: {
+        startGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.winner = null;
+            this.currentRound = 0;
+        },
         attackMonster() {
             this.currentRound++;
             const attackValue = getRandomValue(5, 12)
@@ -52,6 +64,10 @@ const app = Vue.createApp({
             }
             this.attackPlayer();
         },
+        surrender() {
+            this.winner = 'monster';
+        },
+
     },
     watch: {
         playerHealth(value) {
